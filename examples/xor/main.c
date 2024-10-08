3d4ad932ecf825e80f693658b5cbf5aec74388df
erostedt <rostedteric@gmail.com>
2024-10-08 17:24:00 +0200

    add xor example

--- examples/xor/main.c
+++ examples/xor/main.c
@@ -4,7 +4,7 @@
 #include "loss.h"
 #include "matrix.h"
 
-matrix_t make_and_features()
+matrix_t make_xor_features()
 {
     matrix_t features = matrix_alloc(4, 2);
     MATRIX_AT(features, 0, 0) = 0;
@@ -21,21 +21,21 @@ matrix_t make_and_features()
     return features;
 }
 
-matrix_t make_and_targets()
+matrix_t make_xor_targets()
 {
     matrix_t targets = matrix_alloc(4, 1);
     MATRIX_AT(targets, 0, 0) = 0;
-    MATRIX_AT(targets, 1, 0) = 0;
-    MATRIX_AT(targets, 2, 0) = 0;
-    MATRIX_AT(targets, 3, 0) = 1;
+    MATRIX_AT(targets, 1, 0) = 1;
+    MATRIX_AT(targets, 2, 0) = 1;
+    MATRIX_AT(targets, 3, 0) = 0;
     return targets;
 }
 
 int main()
 {
     network_t network = network_alloc(4, (size_t[4]){2, 4, 1}, 3);
-    matrix_t inputs = make_and_features();
-    matrix_t targets = make_and_targets();
+    matrix_t inputs = make_xor_features();
+    matrix_t targets = make_xor_targets();
     for (size_t i = 0; i < 10000; ++i)
     {
         network_train(&network, inputs, targets, 1e-0f);
@@ -43,8 +43,8 @@ int main()
     }
 
     matrix_t pred = network_forward(&network, inputs);
-    printf("%f | %f = %f\n", MATRIX_AT(inputs, 0, 0), MATRIX_AT(inputs, 0, 1), MATRIX_AT(pred, 0, 0));
-    printf("%f | %f = %f\n", MATRIX_AT(inputs, 1, 0), MATRIX_AT(inputs, 1, 1), MATRIX_AT(pred, 1, 0));
-    printf("%f | %f = %f\n", MATRIX_AT(inputs, 2, 0), MATRIX_AT(inputs, 2, 1), MATRIX_AT(pred, 2, 0));
-    printf("%f | %f = %f\n", MATRIX_AT(inputs, 3, 0), MATRIX_AT(inputs, 3, 1), MATRIX_AT(pred, 3, 0));
+    printf("%f ^ %f = %f\n", MATRIX_AT(inputs, 0, 0), MATRIX_AT(inputs, 0, 1), MATRIX_AT(pred, 0, 0));
+    printf("%f ^ %f = %f\n", MATRIX_AT(inputs, 1, 0), MATRIX_AT(inputs, 1, 1), MATRIX_AT(pred, 1, 0));
+    printf("%f ^ %f = %f\n", MATRIX_AT(inputs, 2, 0), MATRIX_AT(inputs, 2, 1), MATRIX_AT(pred, 2, 0));
+    printf("%f ^ %f = %f\n", MATRIX_AT(inputs, 3, 0), MATRIX_AT(inputs, 3, 1), MATRIX_AT(pred, 3, 0));
 }
