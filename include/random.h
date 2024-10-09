65c1236b0004723f533636a5cf7e23f4f1a2a566
Eric Rostedt <rostedteric@gmail.com>
2024-10-09 20:35:45 +0200

    move sampling to random

---
+++ include/random.h
@@ -0,0 +1,17 @@
+#include <math.h>
+#include <stdlib.h>
+
+static float uniform(float min, float max)
+{
+    float x = (float)rand() / RAND_MAX;
+    return min + x * (max - min);
+}
+
+static float normal(float mean, float std)
+{
+    float u1 = uniform(0.0f, 1.0f);
+    float u2 = uniform(0.0f, 1.0f);
+
+    float z0 = sqrtf(-2.0 * logf(u1)) * cosf(2.0 * M_PI * u2);
+    return z0 * std + mean;
+}
