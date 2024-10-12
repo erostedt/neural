fc877b3e00515ebec4f53b0a93d6967690ab0064
Eric Rostedt <rostedteric@gmail.com>
2024-10-12 13:25:07 +0200

    update examples

--- examples/xor/main.c
+++ examples/xor/main.c
@@ -5,33 +5,6 @@
 #include "matrix.h"
 #include "network.h"
 
-matrix_t make_xor_features()
-{
-    matrix_t features = matrix_alloc(4, 2);
-    MATRIX_AT(features, 0, 0) = 0;
-    MATRIX_AT(features, 0, 1) = 0;
-
-    MATRIX_AT(features, 1, 0) = 1;
-    MATRIX_AT(features, 1, 1) = 0;
-
-    MATRIX_AT(features, 2, 0) = 0;
-    MATRIX_AT(features, 2, 1) = 1;
-
-    MATRIX_AT(features, 3, 0) = 1;
-    MATRIX_AT(features, 3, 1) = 1;
-    return features;
-}
-
-matrix_t make_xor_targets()
-{
-    matrix_t targets = matrix_alloc(4, 1);
-    MATRIX_AT(targets, 0, 0) = 0;
-    MATRIX_AT(targets, 1, 0) = 1;
-    MATRIX_AT(targets, 2, 0) = 1;
-    MATRIX_AT(targets, 3, 0) = 0;
-    return targets;
-}
-
 int main()
 {
     srand(37);
@@ -39,8 +12,8 @@ int main()
     spec[0] = (layer_spec_t){2, 4, SIGMOID};
     spec[1] = (layer_spec_t){4, 1, SIGMOID};
     network_t network = network_alloc(4, spec, 2);
-    matrix_t inputs = make_xor_features();
-    matrix_t targets = make_xor_targets();
+    matrix_t inputs = (matrix_t){4, 2, (double[]){0, 0, 1, 0, 0, 1, 1, 1}};
+    matrix_t targets = (matrix_t){4, 1, (double[]){0, 1, 1, 0}};
     for (size_t i = 0; i < 10000; ++i)
     {
         network_train(&network, inputs, targets, 1e-0);
